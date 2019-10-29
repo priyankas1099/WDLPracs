b3ed8d59767c905a39beaa023b60d5e34ea85aa2
farhankapadia <52866509+farhankapadia@users.noreply.github.com>
2019-10-29 13:03:38 +0530

    Add files via upload

---
+++ Q25.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+if (isset($_SESSION['counter'])) {
+    $_SESSION['counter'] = $_SESSION['counter'] + 1;
+} else {
+    $_SESSION['counter'] = 1;
+}
+echo "Page Views Count On This Session:" . $_SESSION['counter'];
